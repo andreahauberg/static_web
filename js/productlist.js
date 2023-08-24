f36bca129651acf3530be78f41222823c359f017
Andrea <132267442+andreahauberg@users.noreply.github.com>
2023-08-24 14:14:52 +0200

    k

--- js/productlist.js
+++ js/productlist.js
@@ -1,17 +1,27 @@
-fetch("https://kea-alt-del.dk/t7/api/products?limit=4")
+fetch("https://kea-alt-del.dk/t7/api/products?limit=100")
     .then((res) => res.json())
     .then((data) => {
         data.forEach(showProduct);
     });
 
-function showProduct(data){
-    const template = document.querySelector("template").content;
-    const copy = template.cloneNode(true);
-    copy.querySelector("h2").textContent = data.productdisplayname;
-    copy.querySelector(".soldout").textContent = data.soldout;
-    copy.querySelector(".discount").textContent = data.discount;
-    copy.querySelector(
-        "img"
-    ).src = `https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp`;
-    document.querySelector("main").appendChild(copy);
-}
+    function showProduct(data){
+        const template = document.querySelector("template").content;
+        const copy = template.cloneNode(true);
+        copy.querySelector("h2").textContent = data.productdisplayname;
+    
+        if (data.soldout === 0) {
+            copy.querySelector(".soldout span").textContent = "Udsolgt";
+        } else {
+            copy.querySelector(".soldout span").textContent = "På Lager";
+        }
+    
+        if (data.discount !== null) {
+            copy.querySelector(".discount span").textContent = data.discount;
+        } else {
+            copy.querySelector(".discount").style.display = "none";
+        }
+    
+        copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp`;
+        document.querySelector("main").appendChild(copy);
+    }
+    
