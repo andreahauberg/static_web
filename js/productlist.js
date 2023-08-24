bf80ac4adb9b53677915f56701619365ca73967c
Andrea <132267442+andreahauberg@users.noreply.github.com>
2023-08-24 11:41:37 +0200

    d

--- js/productlist.js
+++ js/productlist.js
@@ -8,6 +8,8 @@ function showProduct(data){
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
     copy.querySelector("h2").textContent = data.productdisplayname;
+    copy.querySelector(".soldout").textContent = data.soldout;
+    copy.querySelector(".discount").textContent = data.discount;
     copy.querySelector(
         "img"
     ).src = `https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp`;
